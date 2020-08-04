16b24ca5f61291fd9172ade2ad93877e5c9710d5
Rondineli Saad <rondineli.saad@gmail.com>
2020-08-03 21:10:56 -0300

    Criados atributos complexos filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,16 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=35):
+    def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
+        self.filhos =  list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Rondineli')
-    print(Pessoa.cumprimentar(p))
-    print(p.nome)
-    print(p.cumprimentar())
-    p.nome = "Renzo"
-    print(p.nome)
-    print(p.idade)
+    rondineli = Pessoa(nome="Rondineli")
+    alex = Pessoa(nome="alex")
+    miguel = Pessoa(rondineli, alex, nome="Miguel")
+    print(id(rondineli))
+    print(rondineli.cumprimentar())
+    print(rondineli.nome)
+    print(rondineli.idade)
+    for filho in miguel.filhos:
+        print(filho.nome)
